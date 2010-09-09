68f2ac1fa3e774ea7fee6134a4cf6c234acd3bc4
Ben Bleything <ben@bleything.net>
2010-09-09 10:16:50 -0700

    remove apt cookbook

--- apt/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Ben Bleything"
-maintainer_email "ben@estately.com"
-license          "Apache 2.0"
-description      "runs apt-get update"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
