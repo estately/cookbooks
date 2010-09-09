3a303436915dc5cb966263bc80e59c2bfd76767a
Ben Bleything <ben@bleything.net>
2010-09-09 15:25:56 -0700

    add apcupsd cookbook, munin plugin recipe

---
+++ apcupsd/attributes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: apcupsd
+# Attributes:: default
+#
+# Copyright 2010, Estately, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+default.apcupsd.upscable 'usb'
+default.apcupsd.upstype  'usb'
+default.apcupsd.device   ''
