e4fd99ab2cf45f5b01d6bd8ede31142d78d2a0f2
Ben Bleything <ben@bleything.net>
2010-09-21 16:32:51 -0700

    apt: add unattended_upgrades recipe

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Ben Bleything"
 maintainer_email "ben@estately.com"
 license          "Apache 2.0"
-description      "runs apt-get update"
+description      "runs apt-get update; configures automatic security upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
+version          "0.2"
---
+++ apt/recipes/unattended_upgrades.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: apt
+# Recipe:: unattended_upgrades
+#
+# Copyright 2010, Estately, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package "unattended-upgrades"
+
+template "/etc/apt/apt.conf.d/10periodic" do
+  owner "root"
+  group "root"
+  mode  0644
+end
+
+template "/etc/apt/apt.conf.d/50unattended-upgrades" do
+  owner "root"
+  group "root"
+  mode  0644
+end
