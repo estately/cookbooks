34c8c1be0c3b095b3899fad4ff1420512948bd00
Ben Bleything <ben@bleything.net>
2010-10-12 09:58:31 -0700

    apt: add apt::backports; update version and README

--- apt/metadata.rb
+++ apt/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "ben@estately.com"
 license          "Apache 2.0"
 description      "runs apt-get update; configures automatic security upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3"
+version          "0.4"
