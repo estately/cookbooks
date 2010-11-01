70cc14c2907272b05799150a690a59d3a3897bb6
Ben Bleything <ben@bleything.net>
2010-11-01 16:13:27 -0700

    add haproxy recipe

---
+++ haproxy/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: haproxy
+# Recipe:: default
+#
+# Copyright 2010, Estately, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package("haproxy") { action :install }
+
+# Clean up installed crap
+directory "/etc/haproxy" do
+  action    :delete
+  recursive true
+end
+
+# install a config
+template "/etc/haproxy.conf" do
+  owner "root"
+  group "root"
+  mode 0644
+end
