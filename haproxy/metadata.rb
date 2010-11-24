6a0b6fc6a626e600607064f9e454536211b10551
Ben Bleything <ben@bleything.net>
2010-11-23 18:04:28 -0800

    remove haproxy recipe

--- haproxy/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Ben Bleything"
-maintainer_email "ben@estately.com"
-license          "Apache 2.0"
-description      "Installs/Configures haproxy"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1"
-
-depends 'runit'
