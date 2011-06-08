9fb452dda87016d9065c8880334c0ba6c5e2f3d9
Ben Bleything <ben@bleything.net>
2011-06-07 17:32:32 -0700

    memcached: tweaks for chef 0.10

--- memcached/metadata.rb
+++ memcached/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "ben@estately.com"
 license          "Apache 2.0"
 description      "Installs/Configures memcached"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2"
+version          "0.2.1"
