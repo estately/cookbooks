7085b816d648cb46a18a54c8e1cd592d869d1af6
Ben Bleything <ben@bleything.net>
2011-06-14 16:59:26 -0700

    motd: show chef environment

--- motd/metadata.rb
+++ motd/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "ben@estately.com"
 license          "Apache 2.0"
 description      "Rejiggers Ubuntu's motd to include interesting infos"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2"
+version          "0.2.1"
