5429762d9db51fcd640bf477ae44451958339957
Ben Bleything <ben@bleything.net>
2011-07-13 15:49:10 -0700

    import wip kvm recipe

---
+++ kvm/recipes/host.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: kvm
+# Recipe:: host
+#
+# Copyright 2011, Estately, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package "qemu"
+package "qemu-kvm"
+package "libvirt-bin"
+package "ubuntu-vm-builder"
+
+### make all sysadmins able to administer VMs
+group 'libvirtd' do
+  action  :modify
+  members search( :groups, "id:sysadmins" ).first['members']
+end
