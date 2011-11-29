fbacfe813185f4848591843d9bca18032ea0bd45
Aaron Baer <aaron@slyness.org>
2011-11-29 00:06:27 -0800

    Import pdftk version 0.0.1

---
+++ pdftk/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Heavy Water Software Inc."
+maintainer_email "aaron@slyness.org"
+license          "Apache 2.0"
+description      "Installs/Configures pdftk"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
