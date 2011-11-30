aca98981930928a5f2a99a8060ac10a0f1014a58
Aaron Baer <aaron@slyness.org>
2011-11-30 00:25:03 -0800

    Make updates to recipe to only build if software is not already installed

--- librets/metadata.rb
+++ librets/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "Ben Bleything"
-maintainer_email "ben@estately.com"
+maintainer       "Aaron Baer"
+maintainer_email "aaron@slyness.org"
 license          "Apache 2.0"
 description      "Installs librets"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.2.1"
+version          "0.3.0"
