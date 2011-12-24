b7cb8486b834c694f85ec7d4a200634729246280
Sean Escriva <sean.escriva@gmail.com>
2011-12-23 17:00:42 -0800

    depends for updated nginx cookbook

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -4,3 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.4.0"
+
+%w{ build-essential runit }.each do |cb|
+  depends cb
+end
